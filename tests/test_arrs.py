da3f0deefee16e86f57c0ab76ee29a852b0e64f6
olja <matvejevaolga@gmail.com>
2023-04-13 07:14:52 +0300

    Initial commit

---
+++ tests/test_arrs.py
@@ -0,0 +1,23 @@
+import unittest
+from utils import arrs
+
+
+class TestArrs(unittest.TestCase):
+    def test_get(self):
+        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(arrs.get([1], 0, "test"), 1)
+        self.assertNotEqual(arrs.get([1, 2, 3], -1), [])
+
+    def test_slice(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -5), [1, 2, 3])
+        self.assertNotEqual(arrs.my_slice([1, 2, 3], 0), [0])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 0), [1, 2, 3])
+        self.assertEqual(arrs.my_slice([]), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3]), [1, 2, 3])
+        self.assertNotEqual(arrs.my_slice([1, 2, 3], -1), [3, 2, 1])
+
+
+
+
